6a3f43c68e2d097d855a25a7fa4cb60815a97dd7
rillyv <rillyv.dev@gmail.com>
2025-09-23 10:38:42 +0200

    Create Entry Entity and Repository Create EntryType enum

---
+++ src/Entity/Entry.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\EntryType;
+use App\Repository\EntryRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EntryRepository::class)]
+class Entry
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(enumType: EntryType::class)]
+    private ?EntryType $type = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $amount = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?EntryType
+    {
+        return $this->type;
+    }
+
+    public function setType(EntryType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ src/Enum/EntryType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+enum EntryType: string
+{
+    case INCOME = 'income';
+    case EXPENSE = 'expense';
+}
